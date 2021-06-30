6b7e7d519ac12f961defbf8f4481309d48aa6134
mahmoud481 <mah.mohamed48@gmail.com>
2021-06-30 23:29:56 +0200

    add ToDoController, Note Model,Migration and Request

---
+++ app/Http/Controllers/API/ToDoController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\NoteRequest;
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class ToDoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $notes = Note::paginate(4);
+        // return PostResource::collection($posts);
+        // dd($notes);
+        return $notes;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd("create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(NoteRequest $request)
+    {
+        // dd($request);
+        $note = Note::create($request->all());
+        return $note;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // dd("show" . $id);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        dd("edit" . $id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        dd("update" . $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        dd('delete' . $id);
+    }
+}
